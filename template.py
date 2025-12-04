7511981bab569cfcaea81192c5bd022c6ceaae45
aiviaghost <smurfplatta@gmail.com>
2025-12-04 23:14:22 +0100

    Add template

---
+++ template.py
@@ -0,0 +1,20 @@
+import math
+import re
+from collections import Counter, defaultdict
+from functools import reduce
+from itertools import accumulate, combinations, product
+
+with open("input", "r") as f:
+    data = f.readlines()
+
+
+def solve_1():
+    pass
+
+
+def solve_2():
+    pass
+
+
+solve_1()
+solve_2()

66dcec10ab4909546cf3976f4c538e8a3a829987
aiviaghost <smurfplatta@gmail.com>
2025-12-21 23:50:13 +0100

    Solve day 11

---
+++ day-11/solve.py
@@ -0,0 +1,48 @@
+import math
+import re
+from collections import Counter, defaultdict
+from functools import reduce, cache
+from itertools import accumulate, combinations, product
+
+with open("input", "r") as f:
+    data = list(map(str.strip, f.readlines()))
+
+
+adj = {}
+for line in data:
+    u, *vs = line.replace(":", "").split()
+    adj[u] = vs
+
+
+@cache
+def dfs(curr):
+    return 1 if curr == "out" else sum(map(dfs, adj[curr]))
+
+
+@cache
+def dfs2(curr, target, exclude=None):
+    if curr == target:
+        return 1
+    return sum(
+        dfs2(child, target, exclude) for child in adj.get(curr, []) if child != exclude
+    )
+
+
+def solve_1():
+    print(dfs("you"))
+
+
+def solve_2():
+    svr_to_dac = dfs2("svr", "dac", "fft")
+    dac_to_fft = dfs2("dac", "fft")
+    fft_to_out = dfs2("fft", "out", "dac")
+
+    svr_to_fft = dfs2("svr", "fft", "dac")
+    fft_to_dac = dfs2("fft", "dac")
+    dac_to_out = dfs2("dac", "out", "fft")
+
+    print(svr_to_dac * dac_to_fft * fft_to_out + svr_to_fft * fft_to_dac * dac_to_out)
+
+
+solve_1()
+solve_2()
